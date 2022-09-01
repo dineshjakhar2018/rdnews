7b7ab405d83a12148c7b996b03ad465b74504699
DINESH KUMAR JAKHAR <94303321+dineshjakhar2018@users.noreply.github.com>
2022-09-01 08:52:31 +0530

    Update urls.py

--- rdnews/urls.py
+++ rdnews/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from news import views
+from .news import views
 
 urlpatterns = [
     path('',views.index,name='home'),
